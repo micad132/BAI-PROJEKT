30a2a3dc62daebc60118eda1a50fc67b848ad854
Michal <micad132132@gmail.com>
2023-10-21 20:39:57 +0200

    vite init

---
+++ warehouseManagement/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+          WITAM TEST
+      </div>
+  )
+}
+
+export default App
---
+++ warehouseManagement/src/components/spinner.component.tsx
@@ -0,0 +1,5 @@
+import { Spinner } from '@chakra-ui/react'
+
+const SpinnerComponent = () => <Spinner size='xl' />
+
+export default SpinnerComponent;
